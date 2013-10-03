e56a491e82cb47a78b842bafdbe8d3088e12ecf5
Nik Cross <nik.cross@diamond.ac.uk>
2013-10-03 16:06:23 +0100

    Classes ready to supply the structure of classes from their source.

---
+++ src/lab/java/org/one/stone/soup/java/analysis/SourceCode.java
@@ -0,0 +1,20 @@
+package org.one.stone.soup.java.analysis;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SourceCode {
+
+	public class MethodMarker {
+		public String signiture;
+		public int startPosition;
+		public int endPosition;
+	}
+	
+	private List<MethodMarker> methods = new ArrayList<MethodMarker>();
+	private String source;
+	
+	public SourceCode(String source) {
+		this.source = source;
+	}
+}
---
+++ src/lab/java/org/one/stone/soup/java/analysis/TextCookieCutter.java
@@ -0,0 +1,5 @@
+package org.one.stone.soup.java.analysis;
+
+public class TextCookieCutter {
+
+}
