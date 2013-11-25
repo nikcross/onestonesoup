a8d93bdc92f7d862f2d0d3991efc6ac1220145d1
Nik Cross <nik.cross@diamond.ac.uk>
2013-11-25 17:35:00 +0000

    New Remote web service javascript helper

---
+++ src/lab/java/org/one/stone/soup/javascript/helper/RemoteWebServiceAccess.java
@@ -0,0 +1,43 @@
+package org.one.stone.soup.javascript.helper;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.net.URLConnection;
+
+import org.one.stone.soup.core.FileHelper;
+
+public class RemoteWebServiceAccess {
+
+	public static String getURLAsString(String url) throws IOException {
+		URL u = new URL(url);
+		URLConnection c = u.openConnection();
+		return FileHelper.loadFileAsString(c.getInputStream());
+	}
+	
+	public static String getURLAsString(String url,String user,String password) throws IOException {
+		@SuppressWarnings("restriction")
+		String code = new sun.misc.BASE64Encoder().encode((user+":"+password).getBytes());
+		
+		URL u = new URL(url);
+		URLConnection c = u.openConnection();
+		c.setRequestProperty("Authorization", "Basic " + code);
+		return FileHelper.loadFileAsString(c.getInputStream());
+	}
+	
+	public static void getURLAsFile(String url,String fileName) throws IOException {
+		URL u = new URL(url);
+		URLConnection c = u.openConnection();
+		FileHelper.copyInputStreamToFile(c.getInputStream(),new File(fileName));
+	}
+	
+	public static void getURLAsString(String url,String user,String password,String fileName) throws IOException {
+		@SuppressWarnings("restriction")
+		String code = new sun.misc.BASE64Encoder().encode((user+":"+password).getBytes());
+		
+		URL u = new URL(url);
+		URLConnection c = u.openConnection();
+		c.setRequestProperty("Authorization", "Basic " + code);
+		FileHelper.copyInputStreamToFile(c.getInputStream(),new File(fileName));
+	}
+}
