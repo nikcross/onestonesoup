d99a4ecd3dbd258424a3d8191701248bac146142
Nik Cross <nik.cross@diamond.ac.uk>
2013-11-27 17:16:36 +0000

    Added pseudo code to PackageDrive

--- src/lab/java/org/one/stone/soup/javascript/helper/PackageDrive.java
+++ src/lab/java/org/one/stone/soup/javascript/helper/PackageDrive.java
@@ -1,5 +1,14 @@
 package org.one.stone.soup.javascript.helper;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.one.stone.soup.core.FileHelper;
+import org.one.stone.soup.core.JSONHelper;
+
 public class PackageDrive {
 
 	public class PackageDetails {
@@ -21,23 +30,55 @@ public class PackageDrive {
 		private String[] packageDependencies;
 	}
 	
+	private Map<String,PackageDetails> packages = new HashMap<String,PackageDetails> ();
+	
 	//name.version	
 	public String[] findPackages(String matcher) {
-		//TODO
-		return new String[]{};
+		List<String> keys = new ArrayList<String>();
+		for(String key: packages.keySet()) {
+			if(key.matches(matcher)) {
+				keys.add(key);
+			}
+		}
+		return keys.toArray(new String[]{});
 	}
 	
 	public PackageDetails getPackage(String name) {
-		//TODO
-		return null;
+		return packages.get(name);
+	}
+	
+	public void initialise(String fileName) {
+		//EntityTree tree = JSONHelper.fromJSON(fileName);
+		//for each package
+		//	create package
+		//	add to packages
+	}
+	
+	public void persist(String fileName) throws IOException {
+		
+		List<PackageDetails> packageList = new ArrayList<PackageDetails>();
+		for(String key: packages.keySet()) {
+				packageList.add(packages.get(key));
+		}
+		FileHelper.saveStringToFile(JSONHelper.toJSON(packageList), fileName);
 	}
 	
 	public void installPackage(PackageDetails packageDetails) {
 		//TODO
+		//download package into temporary area
+		//unzip package
+		//for each file in package,
+			//backup the current version
+			//copy in the new version
+			//log the change
 	}
 	
 	public PackageDetails createPackage(PackageDetails oldPackageDetails,String name,String version,String homeURL) {
 		//TODO
+		//create new package details
+		//zip the files into a package
+		//upload the package to the homeURL
+		//return the new package details
 		return null;
 	}
 }
